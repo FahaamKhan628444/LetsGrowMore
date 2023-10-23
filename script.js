682015824702d8560cb711bd5a34e2c99c4786e7
Fahaam Khan <122596213+FahaamKhan628444@users.noreply.github.com>
2023-10-23 15:00:29 +0530

    Task-4 User Card

--- script.js
+++ script.js
@@ -1,52 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const userImage = document.getElementById("user-image");
-    const userName = document.getElementById("user-name");
-    const userDescription = document.getElementById("user-description");
-    const userEmail = document.getElementById("user-email");
-    const editUserButton = document.getElementById("edit-user-button");
-    const userModal = document.getElementById("user-modal");
-    const closeUserModal = document.getElementById("close-modal");
-    const saveUserButton = document.getElementById("save-user-button");
-    const newNameInput = document.getElementById("new-name");
-    const newDescriptionInput = document.getElementById("new-description");
-    const newEmailInput = document.getElementById("new-email");
-
-    editUserButton.addEventListener("click", function () {
-        newNameInput.value = userName.textContent;
-        newDescriptionInput.value = userDescription.textContent;
-        newEmailInput.value = userEmail.textContent.replace("Email: ", "");
-        userModal.style.display = "block";
-    });
-
-    closeUserModal.addEventListener("click", function () {
-        userModal.style.display = "none";
-    });
-
-    saveUserButton.addEventListener("click", function () {
-        userName.textContent = newNameInput.value;
-        userDescription.textContent = newDescriptionInput.value;
-        userEmail.textContent = "Email: " + newEmailInput.value;
-        userModal.style.display = "none";
-    });
-
-    // Function to fetch user data from an API (for demonstration purposes)
-    function fetchUserData() {
-        // You can use Fetch or other methods to get data from an API here.
-        // For this example, we'll just use static data.
-        const userData = {
-            name: "John Doe",
-            description: "Web Developer",
-            email: "johndoe@example.com",
-            image: "user.jpg",
-        };
-
-        // Update the user card with fetched data
-        userImage.src = userData.image;
-        userName.textContent = userData.name;
-        userDescription.textContent = userData.description;
-        userEmail.textContent = "Email: " + userData.email;
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
     }
-
-    // Fetch user data when the page loads
-    fetchUserData();
-});
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
